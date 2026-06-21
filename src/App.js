3f01817e410d7581f79b4653592762b86855c8d8
QuocHuy <quochuy@phutungb2b.com>
2026-06-21 13:11:55 +0700

    9.3.1 Form Login - Dàn Layout với Bootstrap 5 | SERIES FULLSTACK - JWT, Node.JS & React

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <div className="app-container">
-        <Nav />
+        {/* <Nav /> */}
         <Switch>
           <Route path="/news">news</Route>
           <Route path="/contact">contact</Route>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,9 +2,36 @@ import "./Login.scss";
 
 const Login = (props) => {
   return (
-    <div className="login-container">
-      Login
-      <button className="btn btn-primary">test boostrap</button>
+    <div className="login-container mt-3">
+      <div className="container">
+        <div className="row">
+          <div className="content-left  col-7 ">
+            <div className="brand">Do Quoc Huy</div>
+            <div className="detail">
+              Do Quoc Huy helps you connect and share with the people in your
+              life
+            </div>
+          </div>
+          <div className="content-right green col-5 d-flex flex-column gap-3 py-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email address or phone number"
+            />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            />
+            <button className="btn btn-primary">Login</button>
+            <span className="text-center">Forgot yout password?</span>
+            <hr />
+            <div className="text-center">
+              <button className="btn btn-success">Create new account</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
