02f2ec6061f26021a313b6d5913e2f482cb800b4
QuocHuy <quochuy@phutungb2b.com>
2026-06-23 10:15:13 +0700

    9.3.3 Form Login Responsive - Tạo Form Cho Mobile/Laptop | SERIES FULLSTACK - JWT, Node.JS & React

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,17 +2,19 @@ import "./Login.scss";
 
 const Login = (props) => {
   return (
-    <div className="login-container mt-3">
+    <div className="login-container px-3 px-sm-0 ">
       <div className="container">
-        <div className="row">
-          <div className="content-left  col-7 ">
+        <div className="row ">
+          <div className="content-left  col-12 d-none col-sm-7 d-sm-block   ">
             <div className="brand">Do Quoc Huy</div>
             <div className="detail">
               Do Quoc Huy helps you connect and share with the people in your
               life
             </div>
           </div>
-          <div className="content-right green col-5 d-flex flex-column gap-3 py-3">
+
+          <div className="content-right  col-12 col-sm-5 d-flex flex-column gap-3 py-3">
+            <div className="brand d-block d-sm-none">Do Quoc Huy</div>
             <input
               type="text"
               className="form-control"
@@ -24,7 +26,11 @@ const Login = (props) => {
               placeholder="Password"
             />
             <button className="btn btn-primary">Login</button>
-            <span className="text-center">Forgot yout password?</span>
+            <span className="text-center">
+              <a className="forgot-password" href="#">
+                Forgot yout password?
+              </a>
+            </span>
             <hr />
             <div className="text-center">
               <button className="btn btn-success">Create new account</button>
