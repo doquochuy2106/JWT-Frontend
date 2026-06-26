30e549bfdfa551c1e9445ef2f70da23ee163554e
QuocHuy <quochuy@phutungb2b.com>
2026-06-26 10:26:23 +0700

    9.4.2 Mock APIs - Tạo API Để Test | SERIES FULLSTACK - JWT, Node.JS & React

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -12,7 +12,7 @@ const Register = (props) => {
 
   useEffect(() => {
     axios
-      .get("https://reqres.in/api/users?page=2")
+      .get("http://localhost:8080/api/test-api")
       .then((data) => {
         console.log("check data axios: ", data);
       })
